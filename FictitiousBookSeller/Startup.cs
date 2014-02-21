0315e4e27f92bed9876ee49926d5da46418fd8a6
PatBaham <pat_baham@hotmail.com>
2014-02-20 20:17:16 -0700

    Initial work - 2/20/2014

---
+++ FictitiousBookSeller/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FictitiousBookSeller.Startup))]
+namespace FictitiousBookSeller
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
